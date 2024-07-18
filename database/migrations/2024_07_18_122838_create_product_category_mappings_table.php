4c9977222d2f3d06245fb147ea12c4f44b18ab84
ibrokxim <nibrohim04@gmail.com>
2024-07-18 17:53:53 +0500

    small bug fixes

---
+++ database/migrations/2024_07_18_122838_create_product_category_mappings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_category_mappings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('products_id');
+            $table->unsignedBigInteger('category_id');
+            $table->integer('is_featured_product')->default(0);
+            $table->integer('display_order')->nullable();
+            $table->foreignId('products_id')->references('id')->on('products')->cascadeOnDelete();
+            $table->foreignId('category_id')->references('id')->on('products')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_category_mappings');
+    }
+};
