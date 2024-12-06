72e0de22953e6a3956be6d1d7292aaafc260c81f
ibrokxim <nibrohim04@gmail.com>
2024-12-06 15:27:25 +0500

    disk_with_manufacturers_and_models function

--- app/Services/Product/ProductService.php
+++ app/Services/Product/ProductService.php
@@ -139,25 +139,78 @@ class ProductService
             $formattedManufacturers[] = $manufacturer;
         }
 
-        $disk_manufacturers_with_models = [
-            "AED" => ["AMG55 (tw)"],
-            "ALUTEC" => ["ADX.01", "ADX.02","Tormenta","DRIVE X","IKENU","GRIP","DYNAMITE","MONSTR", "POISON CUP", "POISON", "RAPTR","SHARK","Singa", "TITAN", "W10","W10X"],
-            "ATS" => ["Competition 2","Passion", "CROSSLIGHT","MIZAR", "Grid","PERFEKTION","RACELIGHT","RADIAL", "Temperament",],
-            "CR" => ["MB-962 AMG", "RX-XH273","TY-FC1734 (300)", "TY-RH5001 (200)"],
-            "DN" => ["AU-832","RX-281","X5-5497", "TY-JC2002 (200)"],
-            "F-POWER" => ["QC1151"],
-            "FR" => ["AU-5131", "AU-5436", "D-5459", "HU-485","TRD-1380", "TY-R2027", "TY-P6067"],
-            "FUEL" => ["D712 Rage", "D720 Heater", "D718 Heater"],
-            "LENSO" => ["Conquista-Karizma", "JAGER-DYNA"],
-            "MOTO METAL" => ["MO977 LINK", "MO977"],
-            "MR" => ["TY-1905 (300)"],
-            "NICHE" => ["M204 Vosso"],
-            "RIAL" => ["CATANIA", "Como", "Davos", "DH", "KIBO","LUCCA","LUGANO","M10","M10X","MILANO", "Murago", "OSLO", "PADUA","QUINTO", "Torino",
-                        "Transporter", "ZAMORA",
-                ],
-            "XD SERIES" => ["XD133 Fusion Off-Road", "XD135 Grenade Off-Road", "XD847 Outbreak", "XD856 Omega", "XD140 RECON", "XD827 ROCKSTAR III"]
+    $result = [
+    "manufacturers" => [
+        [
+            "AED" => ["AMG55 (tw)"]
+        ],
+        [
+            "ALUTEC" => [
+                "ADX.01", "ADX.02", "Tormenta", "DRIVE X", "IKENU",
+                "GRIP", "DYNAMITE", "MONSTR", "POISON CUP", "POISON",
+                "RAPTR", "SHARK", "Singa", "TITAN", "W10", "W10X"
+            ]
+        ],
+        [
+            "ATS" => [
+                "Competition 2", "Passion", "CROSSLIGHT", "MIZAR",
+                "Grid", "PERFEKTION", "RACELIGHT", "RADIAL", "Temperament"
+            ]
+        ],
+        [
+            "CR" => [
+                "MB-962 AMG", "RX-XH273", "TY-FC1734 (300)", "TY-RH5001 (200)"
+            ]
+        ],
+        [
+            "DN" => [
+                "AU-832", "RX-281", "X5-5497", "TY-JC2002 (200)"
+            ]
+        ],
+        [
+            "F-POWER" => ["QC1151"]
+        ],
+        [
+            "FR" => [
+                "AU-5131", "AU-5436", "D-5459", "HU-485",
+                "TRD-1380", "TY-R2027", "TY-P6067"
+            ]
+        ],
+        [
+            "FUEL" => ["D712 Rage", "D720 Heater", "D718 Heater"]
+        ],
+        [
+            "LENSO" => ["Conquista-Karizma", "JAGER-DYNA"]
+        ],
+        [
+            "MOTO METAL" => ["MO977 LINK", "MO977"]
+        ],
+        [
+            "MR" => ["TY-1905 (300)"]
+        ],
+        [
+            "NICHE" => ["M204 Vosso"]
+        ],
+        [
+            "RIAL" => [
+                "CATANIA", "Como", "Davos", "DH", "KIBO",
+                "LUCCA", "LUGANO", "M10", "M10X", "MILANO",
+                "Murago", "OSLO", "PADUA", "QUINTO", "Torino",
+                "Transporter", "ZAMORA"
+            ]
+        ],
+        [
+            "XD SERIES" => [
+                "XD133 Fusion Off-Road", "XD135 Grenade Off-Road",
+                "XD847 Outbreak", "XD856 Omega", "XD140 RECON",
+                "XD827 ROCKSTAR III"
+            ]
+        ]
+    ]
+];
 
-            ];
+        $disk_manufacturers_with_models = json_encode($result, JSON_PRETTY_PRINT);
+
         $disk_models = [
             "AMG55 (tw)","ADX.01", "ADX.02","Tormenta","DRIVE X","IKENU","GRIP","DYNAMITE","MONSTR", "POISON CUP", "POISON", "RAPTR","SHARK","Singa", "TITAN", "W10","W10X",
             "Competition 2","Passion", "CROSSLIGHT","MIZAR", "Grid","PERFEKTION","RACELIGHT","RADIAL", "Temperament","MB-962 AMG", "RX-XH273","TY-FC1734 (300)", "TY-RH5001 (200)",
